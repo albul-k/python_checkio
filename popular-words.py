d6470428a465560fb7aea7c3b39ce01657662504
@albul-k <konstantin.albul@gmail.com>
2020-05-09 23:16:05 +0300

    done

---
+++ popular-words.py
@@ -0,0 +1,30 @@
+def popular_words(text: str, words: list) -> dict:
+    words_list = list(map(lambda word: word.lower(), text.split()))
+    words_dict = {}
+    for itm in words:
+        words_dict[itm] = words_list.count(itm)
+    return words_dict
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0,
+        'near': 0
+    }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
